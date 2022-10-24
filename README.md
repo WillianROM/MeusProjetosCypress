8a3764077a0876c0d322a51c14883e17f02eb2e2
Willian Melo <109000079+WillianROM@users.noreply.github.com>
2022-10-23 22:25:47 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Colinha
+npm init -y
+npm install cypress
+code .
+npm run cypress:open
---
+++ cypress/e2e/testeum.cy.js
@@ -0,0 +1,12 @@
+///<reference types="cypress"/>
+
+describe("Acessar o site", () => {
+    before(() => {
+        cy.visit("https://ticket-box.s3.eu-central-1.amazonaws.com/index.html")
+    })
+
+    it('Quem um dia irá dizer', () => {
+
+    })
+
+})
