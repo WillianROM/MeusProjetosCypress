86eb402299d89f42c0c90a1cf8577006feb1a5c9
Willian Melo <109000079+WillianROM@users.noreply.github.com>
2022-10-24 23:48:25 -0300

    Update testeum.cy.js

--- cypress/e2e/testeum.cy.js
+++ cypress/e2e/testeum.cy.js
@@ -1,6 +1,6 @@
 ///<reference types="cypress"/>
 
-describe("Acessar o site", () => {
+describe('Acessar o site', () => {
     before(() => {
         cy.visit("https://ticket-box.s3.eu-central-1.amazonaws.com/index.html")
     })
