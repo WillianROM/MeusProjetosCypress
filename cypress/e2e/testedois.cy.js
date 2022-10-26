91206ac0940364a1538ad4bcc72837987f17dc88
Willian Melo <109000079+WillianROM@users.noreply.github.com>
2022-10-25 23:39:58 -0300

    Update testedois.cy.js

--- cypress/e2e/testedois.cy.js
+++ cypress/e2e/testedois.cy.js
@@ -7,40 +7,54 @@ describe('Acessar o site',()=>{
             .click()
     })
 
-    describe('Novo teste',()=>{
+
         it('',()=>{
+            cy.get('input#id_campo_nome').should('exist').and('be.visible').and('be.enabled')
 
-            cy.get('input#id_campo_nome')
-                .should('exist')
-                .and('be.visible')
-                .and('be.enabled')
+            //Campos de Texto
+            cy.get('input#id_campo_nome').click().type('João')
 
-            cy.get('input#id_campo_nome')
-                .click()
-                .type('João')
+            cy.xpath('//input[@name="sobrenome"]').click().type('Barro')
+
+            cy.get('input#id_campo_email').click().type('joaobarro@birds.com')
+
+            cy.get('textarea#id_campo_descricao').type('Eu sou um pássaro da família Barro, muito prazer.')
+            
+
+            //Botões de radio
+            cy.get('input#id_Linkedin').check()
+
+            //Checkbox
+            cy.get('input#id_checkbox_selenium').check()
+
+            cy.get('input#id_checkbox_robotFramework').check() 
+
+            cy.get('input#id_checkbox_selenium').uncheck() //Desmarca a caixa de seleção
+
+            //Caixas de Seleção
+            cy.get('select#id_campo_linguagemProgramacaoSimples').select('C#')
+
+
+            cy.get('div#mui-component-select-linguagemProgramacao').type('JavaScript')
+            cy.timeout({timeouy:5000})
+            cy.get('body').type('{enter}')
+
+
+            const tags = ['C#','PYTHON','JAVASCRIPT']
+            tags.forEach(valor =>{
+                cy.get('input[name="multiplaSelecao"]')
+                    .type(valor)
+                    .type('{downArrow}')
+                    .type('{enter}')
+
+            })
 
             
-            cy.xpath('//input[@name="sobrenome"]')
-                .click()
-                .type('Barro')
-            cy.get('input#id_campo_email')
-                .click()
-                .type('joaobarro@birds.com')
-            cy.get('textarea#id_campo_descricao')
-                .type('Eu sou um pássaro da família Barro, muito prazer.')
-            cy.get('input#id_Linkedin')
-                .click()
-            cy.get('select#id_campo_linguagemProgramacaoSimples')
-                .select('JAVASCRIPT')
-            cy.get('div.MuiSelect-root')
-                .click()
-            cy.get('[data-value="2"]')
-                .click()
-        })
-    })
 
-    describe('TIPOS DE VALIDAÇÃO',()=>{
-        it.only('',()=>{
+        })
+ 
+
+        it.skip('TIPOS DE VALIDAÇÃO',()=>{
             let a = 1
             let b = 2
 
@@ -62,6 +76,6 @@ describe('Acessar o site',()=>{
             expect(obj).eql({a:1,b:2})
         })
         
-    })
+   
     
 })
