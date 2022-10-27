08aed36c5f9242120e63d7d11cc0ad14523959d3
Willian Melo <109000079+WillianROM@users.noreply.github.com>
2022-10-27 00:44:59 -0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,12 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: "https://qautomatizado-db08c.web.app/",
+    defaultCommandTimeout: 5000,
+    trashAssetsBeforeRuns: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
+
     },
   },
 });
