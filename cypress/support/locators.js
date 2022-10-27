b0c427539f4b777f960efdf1097ad00d715fb95e
Willian Melo <109000079+WillianROM@users.noreply.github.com>
2022-10-26 23:46:04 -0300

    Create locators.js

---
+++ cypress/support/locators.js
@@ -0,0 +1,13 @@
+const locators = {
+    CAMP_TXT:{
+        INPUT_NAME_FIELD: 'input#id_campo_nome',
+        INPUT_LASTNAME_FIELD: '//input[@name="sobrenome"]',
+        INPUT_EMAIL_FIELD: 'input#id_campo_email',
+        TEXTAREA_DESCRIPTION_FIELD: 'textarea#id_campo_descricao'
+    },
+    RADIO_BUTTON:{
+        INPUT_LINKEDIN_RADIO_BUTTON: "input#id_Linkedin"
+    }
+}
+
+export default locators
