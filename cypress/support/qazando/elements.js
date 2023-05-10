bf448f8a58a5506c3a821fd36c359fd7a4eece67
Willian Melo <109000079+WillianROM@users.noreply.github.com>
2023-05-09 23:21:53 -0300

    Update elements.js

--- cypress/support/qazando/elements.js
+++ cypress/support/qazando/elements.js
@@ -3,12 +3,14 @@ const locators = {
         SECTION_HEADER: 'section#top_header'
     },
     BUTTONS:{
-        BUTTON_SEND_MAIL: 'button[name="subscribe"]'
+        BUTTON_SEND_MAIL: 'button[name="subscribe"]',
+        BUTTON_OK: 'button.swal2-confirm.swal2-styled'
     },
     CAMP_TXT:{
         INPUT_EMAIL:'input[name="EMAIL"]'
     },
     FINAL_VALIDATION: {
+        H2_SUCESS: 'h2#swal2-title',
         DIV_THANK_YOU: 'div#swal2-html-container'
     }
 }
